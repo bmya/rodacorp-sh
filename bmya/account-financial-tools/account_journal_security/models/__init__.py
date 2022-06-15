9ca04925f77d14c7a9824517ff7b416662b0ef67
Daniel Blanco <daniel@blancomartin.cl>
2022-06-15 18:41:15 -0300

    [RM] remove account-financial-tools

--- bmya/account-financial-tools/account_journal_security/models/__init__.py
+++
@@ -1,7 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import res_users
-from . import account_move
-from . import account_journal
--- bmya/account-financial-tools/account_ux/wizards/__init__.py
+++
@@ -1,7 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import account_change_currency
-from . import res_config_settings
-from . import account_validate_account_move
